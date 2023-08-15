da4e0ccd89e9e861d00745b387bb53c0bb7f3e3f
ᴀʙᴅᴜʟʟᴀʜ ᴀʟ ɴᴏᴍᴀɴ ᴘʀɪɴᴄᴇ <prince1000bd@gmail.com>
2023-08-15 22:01:14 +0600

    note.txt file added

---
+++ app/error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center text-black font-bold mt-2'>Something went wrong</h1>
+        </div>
+    );
+};
+
+export default Error;
---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center text-black font-bold mt-2'>Nothing Found 404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
